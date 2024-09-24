aa66fe67651b22ab1caa54fb5dc8fb656dea074e
HarishGowda <gowdaharish006@outlook.in>
2024-09-24 10:50:30 +0530

    Frontend: Stage 2

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <div className="login-container">
+        <h1>LOGIN/SIGN-UP</h1>
+        <div className="login">
+            <h2>LOGIN</h2>
+            <input name='username'  />
+            <div className="labels">Username</div>
+        </div>
+        <div className="vr"></div>
+        <div className="register">
+            <h2>SIGN-UP</h2>
+        </div>
+      </div>
+    </>
   )
 }
 
